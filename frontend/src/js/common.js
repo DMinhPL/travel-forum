b6cd9082744dec00b26bd5c3824d049ddd8c3b89
Minh Pham <minh.pham@3forcom.com>
2020-12-15 23:03:34 +0700

    :pushpin: implement login popup

--- frontend/src/js/common.js
+++ frontend/src/js/common.js
@@ -1,2 +1,9 @@
 // eslint-disable-next-line no-console
 console.log('Common All Page');
+(function($) {
+    $(function() {
+        $('.form-validate').each((i, val) => {
+            $(val).validate();
+        });
+    });
+})(jQuery);
